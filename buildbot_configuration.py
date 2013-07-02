a28e0cf99387e35e28a50aaedb080df5531108a2
Alex Ford <fordas@uw.edu>
2013-07-02 15:39:02 -0700

    Adding build drop and force drop build jobs.

--- buildbot_configuration.py
+++ buildbot_configuration.py
@@ -1,15 +1,22 @@
-slave_list = [ ("big%s" % big, {"max_builds" : 3, "properties" : {"slave_build_cores" : 20}}) for big in xrange(1,4)] + [ ("dig%s" % dig, {"max_builds" : 1, "properties" : {"slave_build_cores" : 20}}) for dig in xrange(1, 25)]
+slave_list = \
+    [("big%s" % big, {"max_builds" : 3, "properties" : {"slave_build_cores" : 20}}) for big in xrange(1,4)] + \
+    [ ("dig%s" % dig, {"max_builds" : 1, "properties" : {"slave_build_cores" : 20}}) for dig in xrange(1, 25)] + \
+    [("buildbot0",  {"max_builds" : 1, "properties" : {"slave_build_cores" : 6}})]
 
-master_repo_url = 'file:///work/fordas/workspace/rosetta_dev'
-slave_repo_url = 'file:///work/fordas/workspace/rosetta_dev'
+master_repo_url = 'ssh://git@github.com/RosettaCommons/main.git'
+slave_repo_url  = 'ssh://git@github.com/RosettaCommons/main.git'
+reference_repo = '/lab/gos/RosettaCommons/main.git'
 
 target_builds = ["release"]
 
 target_branches = {
-  "master"                      : {"extras" : None},
-  "residue_orient"              : {"extras" : None},
-  "pyrosetta_build"             : {"extras" : None},
-  "indexed_structure_store"     : {"extras" : "hdf5"}
+  "master"                      : {"extras" : ""},
+  "asford/indexed_structure_store"     : {"extras" : "hdf5"},
+  "matdes/master"     : {"extras" : ""}
 }
 
 integration_result_dir = "/work/buildbot/integration_results"
+build_result_dir = "/work/buildbot/builds"
+
+binding_python_path = "/work/buildbot/buildbot_env"
+binding_boost_path = "/work/buildbot/opt"
