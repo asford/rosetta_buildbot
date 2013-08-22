5872feba6431e0e5efee02aa1fab30edb5fcc82f
Alex Ford <fordas@uw.edu>
2013-08-22 01:38:37 -0700

    Updating buildbot configuration to support additional builder arguments.

--- buildbot_configuration.py
+++ buildbot_configuration.py
@@ -1,7 +1,16 @@
 slave_list = \
-    [("big%s" % big, {"max_builds" : 3, "properties" : {"slave_build_cores" : 20}}) for big in xrange(1,4)] + \
-    [ ("dig%s" % dig, {"max_builds" : 1, "properties" : {"slave_build_cores" : 20}}) for dig in xrange(1, 25)] + \
-    [("buildbot0",  {"max_builds" : 1, "properties" : {"slave_build_cores" : 6}})]
+    [{
+        "name" : "big1",
+        "args" : {"max_builds" : 1, "properties" : {"slave_build_cores" : 20}},
+        "build_filter" : (lambda b: b.startswith("build"))
+    }] +\
+    [{
+        "name" : "buildbot0",
+        "args" : {"max_builds" : 1, "properties" : {"slave_build_cores" : 6}},
+        "build_filter" : (lambda b: not b.startswith("build"))
+    }]
+    #[("big%s" % big, {"max_builds" : 3, "properties" : {"slave_build_cores" : 20}}) for big in xrange(1,4)] + \
+    #[ ("dig%s" % dig, {"max_builds" : 1, "properties" : {"slave_build_cores" : 20}}) for dig in xrange(1, 25)] + \
 
 master_repo_url = 'ssh://git@github.com/RosettaCommons/main.git'
 slave_repo_url  = 'ssh://git@github.com/RosettaCommons/main.git'
@@ -18,5 +27,5 @@ target_branches = {
 integration_result_dir = "/work/buildbot/integration_results"
 build_result_dir = "/work/buildbot/builds"
 
-binding_python_path = "/work/buildbot/buildbot_env"
+binding_python_path = "/usr/local"
 binding_boost_path = "/work/buildbot/opt"
